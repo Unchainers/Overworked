99868db7831b098fa976d252755ba043a198d80a
SolidGluten <colin.oliver655@gmail.com>
2025-07-22 18:46:42 +0700

    # feat(grind-arena): participant + submission structs

--- src/backend/competition/src/lib.rs
+++ src/backend/competition/src/lib.rs
@@ -1,4 +1,4 @@
-use candid::CandidType;
+use candid::{CandidType, Principal};
 use ic_cdk::export_candid;
 use serde::{Deserialize, Serialize};
 use std::cell::RefCell;
@@ -18,6 +18,37 @@ pub struct Competition {
     pub description: String,
     pub level: Level,
     pub prize_pool: u64, // Amount of CRY-tokens
+    pub category_id: String,
+    pub rules: Vec<String>,
+
+    pub thumbnail: 
+
+    pub started_at: u64,
+    pub ended_at: u64,
+}
+
+#[derive(Clone, Serialize, Deserialize, CandidType)]
+pub struct Participants {
+    pub id: String,
+    pub user_id: Principal,
+    pub competition_id: String,
+    pub score: u64, // Score achieved by the user in the competition
+}
+
+#[derive(Clone, Serialize, Deserialize, CandidType)]
+pub struct Submission {
+    pub id: String,
+    pub user_id: Principal,
+    pub competition_id: String,
+    pub content: String, // Content submitted by the user
+}
+
+#[derive(Serialize, Deserialize, CandidType)]
+pub struct CreateCompetitionInput {
+    pub title: String,
+    pub description: String,
+    pub level: Level,
+    pub prize_pool: u64, // Amount of CRY-tokens
     pub started_at: u64,
     pub ended_at: u64,
     pub category_id: String,
@@ -25,31 +56,66 @@ pub struct Competition {
 }
 
 #[derive(Serialize, Deserialize, CandidType)]
-pub struct Participation {
-    pub user_id: String,
+pub struct CreateParticipantInput {
+    pub user_id: Principal,
     pub competition_id: String,
-    pub score: u64, // Score achieved by the user in the competition
 }
 
 #[derive(Serialize, Deserialize, CandidType)]
-pub struct Submission {
-    pub user_id: String,
+pub struct CreateSubmissionInput {
+    pub user_id: Principal,
     pub competition_id: String,
-    pub content: String, // Content submitted by the user
+    pub content: String,
 }
 
 #[derive(Default, Serialize, Deserialize)]
 pub struct CompetitionState {
     pub competitions: HashMap<String, Competition>,
+    pub participants: HashMap<String, Participants>,
+    pub submissions: HashMap<String, Submission>,
 }
 
 thread_local! {
     static COMPETITIONS: RefCell<CompetitionState> = RefCell::new(CompetitionState::default());
+    static PARTICIPANTS: RefCell<CompetitionState> = RefCell::new(CompetitionState::default());
+    static SUBMISSIONS: RefCell<CompetitionState> = RefCell::new(CompetitionState::default());
 }
 
 #[ic_cdk::query]
 fn get_all_competitions() -> Vec<Competition> {
-    COMPETITIONS.with(|state| state.borrow().competitions.values().cloned().collect())
+    COMPETITIONS.with(|state| state
+        .borrow()
+        .competitions
+        .values()
+        .cloned()
+        .collect()
+    )
+}
+
+#[ic_cdk::query]
+fn get_all_participants(competition_id: String) -> Vec<Participants> {
+    PARTICIPANTS.with(|state| {
+        state
+            .borrow()
+            .participants
+            .values()
+            .filter(|p| p.competition_id == competition_id)
+            .cloned()
+            .collect()
+    })
+}
+
+#[ic_cdk::query]
+fn get_all_submissions(competition_id: String) -> Vec<Submission> {
+    SUBMISSIONS.with(|state| {
+        state
+            .borrow()
+            .submissions
+            .values()
+            .filter(|s| s.competition_id == competition_id)
+            .cloned()
+            .collect()
+    })
 }
 
 export_candid!();
