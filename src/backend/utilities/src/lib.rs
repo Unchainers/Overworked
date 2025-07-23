d895b2f7b645047d8939904169dbb0d8ebbe0906
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-07-23 16:38:37 +0700

    # fix remove chrono

--- src/backend/utilities/src/lib.rs
+++ src/backend/utilities/src/lib.rs
@@ -1,4 +1,4 @@
-use time::{format_description::well_known::Rfc3339, OffsetDateTime, UtcOffset};
+use time::{OffsetDateTime, UtcOffset, format_description::well_known::Rfc3339};
 use uuid::Builder;
 
 pub fn now() -> String {
@@ -8,20 +8,15 @@ pub fn now() -> String {
 }
 
 pub fn custom_time_string() -> String {
-let fmt = time::format_description::parse("[year]-[month]-[day] [hour]:[minute]:[second]").unwrap();
-    OffsetDateTime::now_utc()
-        .format(&fmt)
-        .unwrap()
-        
+    let fmt =
+        time::format_description::parse("[year]-[month]-[day] [hour]:[minute]:[second]").unwrap();
+    OffsetDateTime::now_utc().format(&fmt).unwrap()
 }
 
 pub fn convert_to_utc(time_string: String) -> String {
     let dt = OffsetDateTime::parse(&time_string, &Rfc3339).expect("Failed to parse time string");
 
-    dt
-        .to_offset(UtcOffset::UTC)
-        .format(&Rfc3339)
-        .unwrap()
+    dt.to_offset(UtcOffset::UTC).format(&Rfc3339).unwrap()
 }
 
 pub fn convert_to_tz(time_string: String, offset: f32) -> String {
@@ -30,10 +25,7 @@ pub fn convert_to_tz(time_string: String, offset: f32) -> String {
     let total_seconds = (offset * 3600.00) as i32;
     let custom_offset = UtcOffset::from_whole_seconds(total_seconds).expect("Invalid offset");
 
-    dt
-        .to_offset(custom_offset)
-        .format(&Rfc3339)
-        .unwrap()
+    dt.to_offset(custom_offset).format(&Rfc3339).unwrap()
 }
 
 pub fn generate_uuid() -> String {
