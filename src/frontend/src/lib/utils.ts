8b9c9783def9edf4fe8eb3ada829f611899dd761
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-08-18 00:08:58 +0700

    # feat compressFile function

--- src/frontend/src/lib/utils.ts
+++ src/frontend/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { StoredFile } from "../../../declarations/storage/storage.did";
+import imageCompression, { Options } from "browser-image-compression";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -36,4 +37,49 @@ export function convertToFile(fetchedFile: StoredFile): File {
   return new File([new Uint8Array(fetchedFile.data)], fetchedFile.name, {
     type: fetchedFile.mime_type ?? "",
   });
+}
+
+export async function compressFile({
+  file,
+  maxSizeMB = 2,
+  maxWidthOrHeight,
+  onProgress = (progress: number) => console.log(progress),
+  useWebWorker = true,
+  libURL = "https://cdn.jsdelivr.net/npm/browser-image-compression/dist/browser-image-compression.js",
+  preserveExif = true,
+  signal = undefined,
+  maxIteration = 10,
+  alwaysKeepResolution = true,
+}: {
+  file: File;
+  maxSizeMB?: number;
+  maxWidthOrHeight?: number;
+  onProgress: (progress: number) => void;
+  useWebWorker: boolean;
+  libURL: string;
+  preserveExif: boolean;
+  signal: AbortSignal | undefined;
+  maxIteration: number;
+  alwaysKeepResolution: boolean;
+}): Promise<File> {
+  const options: Options = {
+    maxSizeMB,
+    maxWidthOrHeight,
+
+    onProgress,
+    useWebWorker,
+    libURL,
+    preserveExif,
+
+    signal,
+
+    maxIteration,
+    fileType: file.type,
+    initialQuality: 1,
+    alwaysKeepResolution,
+  };
+
+  const compressedFile = await imageCompression(file, options);
+
+  return compressedFile;
 }
